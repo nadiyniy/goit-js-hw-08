3d6472095c766f5ea69128a1472920f3210111b9
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-09-24 18:22:34 +0300

    update

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -6,7 +6,6 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 
 // Change code below this line
 
-console.log(galleryItems);
 const listEl = document.querySelector('.gallery');
 
 const galleryImage = galleryItems
