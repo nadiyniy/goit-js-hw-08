5fbc3d5ca0b93b410e82a22046f670155d40aece
nadiyniy <140396602+nadiyniy@users.noreply.github.com>
2023-09-25 13:12:31 +0300

    finish task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -5,12 +5,42 @@ const form = document.querySelector('.feedback-form');
 const emailInput = form.querySelector('input[name="email"]');
 const messageInput = form.querySelector('textarea[name="message"]');
 
-const onSaveForm = throttle(() => {
-  const saveData = {
+let saveData;
+
+function loadPage() {
+  saveData = loadFromLS('SAVE_KEY') || {};
+  emailInput.value = saveData.email || '';
+  messageInput.value = saveData.message || '';
+}
+
+loadPage();
+
+function loadFromLS(key) {
+  const saveData = localStorage.getItem(key);
+
+  try {
+    return JSON.parse(saveData);
+  } catch {
+    return saveData;
+  }
+}
+
+const onSubmiteFormRemove = e => {
+  e.preventDefault();
+  console.log(saveData);
+  e.target.reset();
+  localStorage.removeItem('SAVE_KEY');
+  data = {};
+};
+
+const onInputForm = throttle(() => {
+  saveData = {
     email: emailInput.value,
     message: messageInput.value,
   };
+
   localStorage.setItem('SAVE_KEY', JSON.stringify(saveData));
 }, 500);
 
-form.addEventListener('input', onSaveForm);
+form.addEventListener('input', onInputForm);
+form.addEventListener('submit', onSubmiteFormRemove);
